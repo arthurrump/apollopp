e05becf3cc299d7ff961c2af8027a5fa12df8cdc
Arthur Rump <hello@arthurrump.com>
2024-09-19 13:28:24 +0200

    Add nix shell with required dependencies

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devShells.default = pkgs.mkShell {
+          name = "apollopp";
+          nativeBuildInputs = with pkgs; [
+            # For helper scripts
+            python3
+            # For translating Processing projects
+            processing
+            # For running the extractors
+            maven
+            rascal
+            # For running the graphmatcher
+            dotnet-sdk_8
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+        # TODO: Override rascal version
+        # overlays.default = final: prev: {
+
+        # };
+      };
+    };
+}
